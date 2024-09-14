690e8f2d2a3c2e56d74538e226931d98931462bb
sertschgi <seppi@neureiter.bayern>
2024-09-14 17:55:15 +0200

    14.09.2024/17:55#0

--- web/src/scripts/fetch_html/index.ts
+++ web/src/scripts/fetch_html/index.ts
@@ -1,12 +1,8 @@
-import {Child} from "@tauri-apps/api/shell";
-
 export function fetch_html(self: & HTMLElement, path: & string, after = () => {}) {
     fetch(path)
         .then(response => response.text())
         .then(data => {
-                const TEMPLATE = document.createElement('div');
                 const SHADOW_ROOT = self.attachShadow({mode: 'open'});
-                // TEMPLATE.innerHTML = data;
                 try {
                     SHADOW_ROOT.innerHTML = data;
                 }
